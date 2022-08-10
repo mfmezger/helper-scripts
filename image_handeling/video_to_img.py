204147fc5bb3778c0fb585c5e68e7915b8df15b7
Marc Mezger <marc.mezger@gmail.com>
2022-08-10 20:28:32 +0200

    New Scripts and Reordering.

---
+++ image_handeling/video_to_img.py
@@ -0,0 +1,17 @@
+import cv2
+from pathlib import Path
+
+
+
+name = "a27ce183-8fcc-4ce0-9fea-52b70a2b49e2.mp4"
+vidcap = cv2.VideoCapture(name)
+Path("output").mkdir(parents=True, exist_ok=True)
+success,image = vidcap.read()
+count = 0
+n = name.split(".")[0]
+if count < 50:
+    while success:
+        cv2.imwrite(f"output/{n}_{count}.jpg", image)     # save frame as JPEG file      
+        success,image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
